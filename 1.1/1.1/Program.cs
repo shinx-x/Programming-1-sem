67b678ca3a6e22e4dbc4a35c1d977ff1804b140a
shinx-x <lanamonthorova@gmail.com>
2024-12-11 14:07:18 +0300

    Add files via upload

---
+++ 1.1/1.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Простые числа, не превосходящие " + n + ":");
+        for (int i = 2; i <= n; i++)
+        {
+            if (IsPrime(i))
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            } 
+        }
+        return true;
+    }
+}
---
+++ 1.1/1.1/Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._1
+{
+    public class Test
+    {
+        public object Assert { get; private set; }
+
+        public List<int> Chinazes(int n)
+        {
+            var result = new List<int>();
+            int j = 0;
+            for (int i = 2; i < n; i++)
+            {
+                for (j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        break;
+                    }
+                }
+
+                if (j > Math.Sqrt(i))
+                {
+                    result.Add(i);
+                }
+                    
+            }
+
+            return result;
+        }
+        
+        public void Setup()
+        {
+        }
+
+        public void Test1()
+        {
+            Assert.AreEqual(0, Chinazes(1).Count);
+        }        
+    }    
+}
